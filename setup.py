22e12387c94308b475fc052bfd870b94c0b7e3c7
LucasChun <77332677+LucasChun@users.noreply.github.com>
2022-05-06 16:10:38 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         'py-AutoClean',
         'ujson',
-        'datawig',
+        'datawig==0.2.0',
         'numpy==1.21.6',
         'pandas==1.3.0',
         'torch'
