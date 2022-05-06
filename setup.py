cc15c67a6dd0da53e0e910bcba27940d22e28c56
LucasChun <hyeonu.chun@gmail.com>
2022-05-06 15:39:14 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='AutoClean',
+    version='0.0.1',
+    description='pip install test',
+    url='https://github.com/LucasChun/SSU_AutoData.git',
+    author='chunhyeonu',
+    author_email='hyeonu.chun@gmail.com',
+    license='hyeonu',
+    packages=['AutoClean'],
+    zip_safe=False,
+    install_requires=[
+        'py-AutoClean',
+        'ujson',
+        'datawig',
+        'torch',
+        'numpy==1.21.6',
+        'pandas==1.3.0'
+    ]
+)
