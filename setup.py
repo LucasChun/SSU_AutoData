da268fd5aef0776ce49f0f50b718fc48c1050bda
LucasChun <77332677+LucasChun@users.noreply.github.com>
2022-05-11 23:35:09 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='AutoClean',
+    name='AutoDataClean',
     version='0.0.1',
     description='pip install test',
     url='https://github.com/LucasChun/SSU_AutoData.git',
     author='chunhyeonu',
     author_email='hyeonu.chun@gmail.com',
     license='hyeonu',
-    packages=['AutoClean', 'datawig'],
+    packages=['AutoDataClean'],
     zip_safe=False,
     install_requires=[
         'loguru',
